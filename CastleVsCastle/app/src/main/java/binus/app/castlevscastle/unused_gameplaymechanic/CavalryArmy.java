a0a1e46900d6e9ee05859ba90d60df16f5fe3e93
SKK-Veyron <70260694+SKK-Veyron@users.noreply.github.com>
2021-07-24 05:29:00 +0700

    Delete CastleVsCastle/app/src/main/java/binus/app/castlevscastle/unused_gameplaymechanic directory

--- CastleVsCastle/app/src/main/java/binus/app/castlevscastle/unused_gameplaymechanic/CavalryArmy.java
+++
@@ -1,8 +0,0 @@
-package binus.app.castlevscastle.unused_gameplaymechanic;
-
-public class CavalryArmy extends Army{
-
-    public CavalryArmy() {
-        this.ArmyType = Army.CAVALRY;
-    }
-}
--- CastleVsCastle/app/src/main/java/binus/app/castlevscastle/unused_gameplaymechanic/InfantryArmy.java
+++
@@ -1,9 +0,0 @@
-package binus.app.castlevscastle.unused_gameplaymechanic;
-
-public class InfantryArmy extends Army{
-
-
-    public InfantryArmy() {
-        this.ArmyType = Army.INFANTRY;
-    }
-}
